df9baeea5f5d7336567eb2da78bed7690f6da9a7
Hugo <hugo.vergnes@gmail.com>
2021-02-15 16:30:20 -0800

    removed clunky eval and added ogb metrics

--- code/train.py
+++ code/train.py
@@ -3,6 +3,7 @@ import json
 import numpy as np
 from ogb.graphproppred import PygGraphPropPredDataset, Evaluator
 import os
+from tqdm import tqdm
 # from torch.utils.tensorboard import SummaryWriter
 # writer = SummaryWriter()
 
@@ -24,41 +25,66 @@ print('Device: {}'.format(device))
 with open('parameters.json') as json_file:
     parameters = json.load(json_file)
 
-def train(model, train_loader, device):
-    model.train()
+# def train(model, train_loader, device):
+#     model.train()
 
-    loss_all = 0
-    for data in train_loader:
-        data = data.to(device)
-        optimizer.zero_grad()
-        output, _ = model(data)
-        loss = F.nll_loss(output, data.y)
-        loss.backward()
-        loss_all += data.num_graphs * loss.item()
-        optimizer.step()
-        for name, param in model.named_parameters():
-            # if 'pan_pool_weight' in name:
-            #     param.data = param.data.clamp(0, 1)
-            if 'panconv_filter_weight' in name:
-                param.data = param.data.clamp(0, 1)
-            if 'panpool_filter_weight' in name:
-                param.data = param.data.clamp(0, 1)
-    return loss_all / len(train_loader.dataset)
+#     loss_all = 0
+#     for data in train_loader:
+#         data = data.to(device)
+#         optimizer.zero_grad()
+#         output, _ = model(data)
+#         loss = F.nll_loss(output, data.y)
+#         loss.backward()
+#         loss_all += data.num_graphs * loss.item()
+#         optimizer.step()
+#         for name, param in model.named_parameters():
+#             # if 'pan_pool_weight' in name:
+#             #     param.data = param.data.clamp(0, 1)
+#             if 'panconv_filter_weight' in name:
+#                 param.data = param.data.clamp(0, 1)
+#             if 'panpool_filter_weight' in name:
+#                 param.data = param.data.clamp(0, 1)
+#     return loss_all / len(train_loader.dataset)
 
 
-def test(model, loader, device):
+def eval(model, loader, device, evaluator):
     model.eval()
 
-    correct = 0
-    loss = 0.0
-    for data in loader:
-        data = data.to(device)
-        out, _ = model(data)
-        pred = out.max(dim=1)[1]
-        correct += pred.eq(data.y).sum().item()
-        label = data.y.squeeze(1)
-        loss += F.nll_loss(out, label).item()*data.num_graphs
-    return correct / len(loader.dataset), loss/len(loader.dataset)
+    # correct = 0
+    # loss = 0.0
+
+    y_true = []
+    y_pred = []
+
+    for step, batch in enumerate(tqdm(loader, desc="Iteration")):
+        batch = batch.to(device)
+
+        if batch.x.shape[0] == 1:
+            pass
+        else:
+            with torch.no_grad():
+                pred, _ = model(batch)
+
+            pred = pred.max(dim=1)[1]
+            y_true.append(batch.y.view(pred.shape).detach().cpu())
+            y_pred.append(pred.detach().cpu())
+
+    y_true = torch.cat(y_true, dim = 0).numpy().reshape(-1, 1)
+    y_pred = torch.cat(y_pred, dim = 0).numpy().reshape(-1, 1)
+
+    input_dict = {"y_true": y_true, "y_pred": y_pred}
+
+    return evaluator.eval(input_dict)
+    # for data in loader:
+    #     data = data.to(device)
+    #     with torch.no_grad():
+    #         out, _ = model(data)
+    #     pred = out.max(dim=1)[1]
+    #     label = data.y.squeeze(1)
+    #     correct += pred.eq(label).sum().item()
+    #     label = data.y.squeeze(1)
+    #     loss += F.nll_loss(out, label).item()*data.num_graphs
+    # return correct / len(loader.dataset), loss/len(loader.dataset)
 
 
 datasetname = parameters["dataset_name"]
@@ -115,6 +141,7 @@ num_graph = len(dataset)
 num_edge = num_edge*1.0/num_graph
 num_node = num_node*1.0/num_graph
 
+# cls_criterion = torch.nn.BCEWithLogitsLoss()
 ## train model
 for run in range(runs):
     evaluator = Evaluator(parameters["dataset_name"])
@@ -142,9 +169,13 @@ for run in range(runs):
     ## train model
     model = PAN(num_node_features,num_classes,nhid=nhid,ratio=pool_ratio,filter_size=filter_size).to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    #uses scheduler for training only
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=parameters["epochs"], 
+    eta_min=10e-7)
 
     for epoch in range(epochs):
         # training
+        print(f"train/ Epoch:{epoch}")
         model.train()
         loss_all = 0
         for data in train_loader:
@@ -156,6 +187,7 @@ for run in range(runs):
             loss.backward()
             loss_all += data.num_graphs * loss.item()
             optimizer.step()
+            scheduler.step()
             for name, param in model.named_parameters():
                 # if 'pan_pool_weight' in name:
                 #     param.data = param.data.clamp(0, 1)
@@ -165,24 +197,27 @@ for run in range(runs):
                     param.data = param.data.clamp(0, 1)
         loss = loss_all / len(train_loader.dataset)   
         train_loss[run,epoch] = loss
+        train_perf = eval(model, train_loader, device, evaluator)
         # validation
-        val_acc_1, val_loss_1 = test(model,val_loader,device)
-        val_loss[run,epoch] = val_loss_1
-        val_acc[run,epoch] = val_acc_1
-        print('Run: {:02d}, Epoch: {:03d}, Val loss: {:.4f}, Val acc: {:.4f}'.format(run+1,epoch+1,val_loss[run,epoch],val_acc[run,epoch]))
-        if val_loss_1 < min_loss[run]:
-            # save the model and reuse later in test
+        validation_perf = eval(model, val_loader, device, evaluator)
+        # Test
+        test_perf = eval(model, test_loader, device, evaluator)
+        # val_loss[run,epoch] = val_loss_1
+        # val_acc[run,epoch] = val_acc_1
+        # print('Run: {:02d}, Epoch: {:03d}, Val loss: {:.4f}, Val acc: {:.4f}'.format(run+1,epoch+1,val_loss[run,epoch],val_acc[run,epoch]))
+        if validation_perf < min_loss[run]:
+            # save the model dand reuse later in test
             torch.save(model.state_dict(), 'latest.pth')
-            min_loss[run] = val_loss_1
+            min_loss[run] = validation_perf
 
     # test
     model.load_state_dict(torch.load('latest.pth'))
-    test_acc[run], _ = test(model,test_loader,device)
-    print('==Test Acc: {:.4f}'.format(test_acc[run]))
+    test_perf = eval(model, test_loader, device, evaluator)
+    print(f'==Test perf: {test_perf}')
 
-print('==Mean Test Acc: {:.4f}'.format(np.mean(test_acc)))
+# print('==Mean Test Acc: {:.4f}'.format(np.mean(test_acc)))
 
-t1 = time.time()
-sv = datasetname + '_pcpa_runs' + str(runs) + '_phi' + str(phi) + '_time' + str(t1) + '.mat'
-sio.savemat(sv,mdict={'test_acc':test_acc,'val_loss':val_loss,'val_acc':val_acc,'train_loss':train_loss,'filter_size':filter_size,'learning_rate':learning_rate,'weight_decay':weight_decay,'nhid':nhid,'batch_size':batch_size,'epochs':epochs})
+# t1 = time.time()
+# sv = datasetname + '_pcpa_runs' + str(runs) + '_phi' + str(phi) + '_time' + str(t1) + '.mat'
+# sio.savemat(sv,mdict={'test_acc':test_acc,'val_loss':val_loss,'val_acc':val_acc,'train_loss':train_loss,'filter_size':filter_size,'learning_rate':learning_rate,'weight_decay':weight_decay,'nhid':nhid,'batch_size':batch_size,'epochs':epochs})
 
