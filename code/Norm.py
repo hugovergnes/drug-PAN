8bcb2f75818432b110f81ea7f9254df4b549b35d
Hugo <hugo.vergnes@gmail.com>
2021-02-21 09:36:50 -0800

    added GraphNorm

---
+++ code/Norm.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+from torch_scatter import scatter_mean
+
+
+class Norm(nn.Module):
+
+    def __init__(self, norm_type, hidden_dim=300, print_info=None):
+        super(Norm, self).__init__()
+        assert norm_type in ['bn', 'gn', None]
+        self.norm = None
+        self.print_info = print_info
+        if norm_type == 'bn':
+            self.norm = nn.BatchNorm1d(hidden_dim)
+        elif norm_type == 'gn':
+            self.norm = norm_type
+            self.alpha = nn.Parameter(torch.ones(hidden_dim))
+            self.weight = nn.Parameter(torch.ones(hidden_dim))
+            self.bias = nn.Parameter(torch.zeros(hidden_dim))
+        
+        self.__eps = 10e-10 #Numerical Stability
+
+    # graph
+    # tensor: node features
+    # def forward(self, graph, tensor, print_=False):
+    def forward(self, x, batch):
+        #### CODE FROM ORIGINAL REPO, but confused about it
+        # if self.norm is None:
+        #     return tensor
+        # if type(self.norm) != str:
+        #     return self.norm(tensor)
+        
+        per_graph_mean = scatter_mean(x, index=batch, dim=0) 
+        shifted = x - self.alpha * per_graph_mean[batch]
+        sigma_2 = scatter_mean(torch.pow(shifted, 2), index=batch, dim=0) + self.__eps
+        return self.weight * shifted / torch.sqrt(sigma_2[batch]) + self.bias
