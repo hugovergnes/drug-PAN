b3f34f47971c7d1f906067b0eda69c7925b44a9f
Hugo <hugo.vergnes@gmail.com>
2021-03-14 23:01:28 -0700

    added save best val loss

--- code/lightning_train.py
+++ code/lightning_train.py
@@ -23,35 +23,44 @@ filter_size = parameters["maximum_path_size"]+1
 learning_rate = parameters["learning_rate"]
 weight_decay = parameters["weight_decay"]
 pool_ratio = parameters["pool_ratio"]
-topk_ratio = parameters['topk_ratio']
+pan_pool_weight = parameters['pan_pool_weight'] # importance weight on global matrix X
+# topk_ratio = parameters['topk_ratio']
 nhid = parameters["nhid"]
 epochs = parameters["epochs"]
 criterion_pos_weight = parameters['criterion_pos_weight']
 LearningRateMonitor_Params = {'logging_interval': 'epoch'}
 
+checkpoint_callback = ModelCheckpoint(
+    monitor='validation_loss',
+    save_top_k=1,
+    mode='min',
+    dirpath='models/',
+    filename='PAN-{epoch:02d}-{validation_loss:.2f}-{rocauc_eval:.2f}',
+    period=1
+)
+
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print('Device: {}'.format(device))
 
-model = LightningPAN(
-    num_node_features=9, num_classes=1, nhid=nhid, 
-    pool_ratio=pool_ratio, topk_ratio=topk_ratio, 
-    criterion_pos_weight=criterion_pos_weight, filter_size=filter_size)
+model = LightningPAN(9, 1, nhid=nhid, 
+                     ratio=pool_ratio, pan_pool_weight=pan_pool_weight,
+                     criterion_pos_weight=criterion_pos_weight,
+                     filter_size=filter_size)
 
 lr_logger = LearningRateMonitor(**LearningRateMonitor_Params)
-neptune_logger = NeptuneLogger(
-                api_key="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vdWkubmVwdHVuZS5haSIsImFwaV91cmwiOiJodHRwczovL3VpLm5lcHR1bmUuYWkiLCJhcGlfa2V5IjoiMDk5YjVmYzYtNTU0My00MzhkLWJiYTAtMGM4ZGVhZmEyMTZiIn0=",
-                project_name='hvergnes/PAN',
-                close_after_fit=False,
-                params=parameters, # your hyperparameters, immutable
-                tags=['PAN', 'change pool, topk, pos_weight, batch_size', 'fix epoch end'],  # tags
-                upload_source_files=["parameters.json", "lightning_model.py"]
-                )
-
+# neptune_logger = NeptuneLogger(
+#                 api_key="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vdWkubmVwdHVuZS5haSIsImFwaV91cmwiOiJodHRwczovL3VpLm5lcHR1bmUuYWkiLCJhcGlfa2V5IjoiMDk5YjVmYzYtNTU0My00MzhkLWJiYTAtMGM4ZGVhZmEyMTZiIn0=",
+#                 project_name='hvergnes/PAN',
+#                 close_after_fit=False,
+#                 params=parameters, # your hyperparameters, immutable
+#                 tags=['PAN', 'Clean code', 'Only first conv see edge weights', 'Pool Ratio=0.2'],  # tags
+#                 upload_source_files=["parameters.json", "lightning_model.py"]
+#                 )
 
 trainer = Trainer(
     max_epochs=epochs,
-    logger=neptune_logger,
-    callbacks=[lr_logger],
+    # logger=neptune_logger,
+    callbacks=[lr_logger, checkpoint_callback],
     # fast_dev_run=True,
 )
 
